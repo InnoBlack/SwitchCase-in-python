d4659bb516083221f1895456884bacd7e76a7f30
Matan <108790837+InnoBlack@users.noreply.github.com>
2023-11-04 10:08:50 +0200

    Add files via upload

---
+++ SwitchCase.py
@@ -0,0 +1,27 @@
+class SwitchCase:
+    class Switch:
+        mylist = []
+
+        def __init__(self, casee):
+            SwitchCase.Switch.mylist.append(casee)
+
+        def __enter__(self):
+            pass
+
+        def __exit__(self, exc_type, exc_value, traceback):
+            SwitchCase.Switch.mylist.clear()
+
+    class Case:
+        def __init__(self, check):
+            self.check = check
+            self.true = SwitchCase.Switch.mylist[0] == self.check
+            self.false = SwitchCase.Switch.mylist[0] != self.check
+
+number = 1
+
+switch = SwitchCase.Switch
+case = SwitchCase.Case
+
+with switch(number):
+    if case(5).true:
+        print("test")
